dc9406ab2dbc8d5710fc6e26fe76b577ce7db6fe
joaosantos004 <j.limasantos@eagles.oc.edu>
2025-09-26 21:49:19 -0500

    add backend files

---
+++ routes/course.routes.js
@@ -0,0 +1,34 @@
+module.exports = app => {
+  const courses = require("../controllers/course.controller.js");
+
+  var router = require("express").Router();
+
+  // Create a new Course
+  router.post("/", courses.create);
+
+  // Retrieve all Courses
+  router.get("/", courses.findAll);
+
+  // Retrieve courses by department
+  router.get("/department/:dept", courses.findByDepartment);
+
+  // Retrieve courses by level
+  router.get("/level/:level", courses.findByLevel);
+
+  // Retrieve courses by credit hours
+  router.get("/hours/:hours", courses.findByCreditHours);
+
+  // Retrieve a single Course with course_number
+  router.get("/:course_number", courses.findOne);
+
+  // Update a Course with course_number
+  router.put("/:course_number", courses.update);
+
+  // Delete a Course with course_number
+  router.delete("/:course_number", courses.delete);
+
+  // Delete all Courses
+  router.delete("/", courses.deleteAll);
+
+  app.use('/api/courses', router);
+};
